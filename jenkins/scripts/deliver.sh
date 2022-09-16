45ac45d4e3de259ff6dd0f6f8885ce914886d83b
Fajar-Islami <ahmadfajarislami@protonmail.com>
2022-09-16 23:41:39 +0700

    :hammer: update jenkinsfile deploy stage

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env sh
 
-echo 'Building Docker Image'
-# set -x
-# docker build -f dockerfile-app . --tag simple-pyhton:1.0.0
-# set +x
+echo 'Installing depedencies'
+set -x
+pip3 install --user --upgrade -r ./config/requirements.txt
+set +x
 
-echo 'Make Container'
-# set -x
-# make container
-# set +x
+echo 'Running REST'
+set -x
+python3 -m uvicorn sources.main:app --reload --port 3000 
+& sleep 1
+echo $! > .pidfile
+set +x
 
 echo 'Now...'
 echo 'Visit http://localhost:3000/docs to see your RestAPI application in action.'
