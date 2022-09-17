672e536686961ccc57452b1b12ca449e174b70b4
Fajar-Islami <ahmadfajarislami@protonmail.com>
2022-09-17 22:47:03 +0700

    update readme, cleaning build.sh jenkinsfile

--- jenkins/scripts/cleaningbuild.sh
+++ jenkins/scripts/cleaningbuild.sh
@@ -7,25 +7,3 @@ docker stop simple-pyhton &&
 set +x
 
 echo 'DONE........'
-
-# echo 'Add to staging'
-# set -x
-# git config --global user.email "ahmadfajarislami@protonmail.com"
-# git config --global user.name "Fajar-Islami"
-# git add .
-# set +x
-
-# echo 'Commit'
-# set -x
-# git commit -am 'push to heroku'
-# set +x
-
-# echo 'Push to Heroku'
-# set -x
-# git remote -v
-# git branch
-# git push -u heroku HEAD:master
-# set +x
-
-
-# echo 'DONE........'
