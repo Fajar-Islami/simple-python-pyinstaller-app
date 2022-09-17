aecc5e434b0cd8d7eae6219fe16a3209660de1a2
Fajar-Islami <ahmadfajarislami@protonmail.com>
2022-09-17 17:31:44 +0700

    :hammer: add push.sh

--- jenkins/scripts/push.sh
+++ jenkins/scripts/push.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env sh
 
 echo 'Add to staging'
-set -x
-git add .
-set +x
+# set -x
+# git add .
+# set +x
 
 echo 'Commit'
-set -x
-git commit -am 'push to heroku'
-set +x
+# set -x
+# git commit -am 'push to heroku'
+# set +x
 
 echo 'Push to Heroku'
-set -x
-git push -u heroku master
-set +x
+# set -x
+# git push -u heroku master
+# set +x
 
-echo 'Now...'
-echo 'Visit http://localhost:3000/docs to see your RestAPI application in action.'
