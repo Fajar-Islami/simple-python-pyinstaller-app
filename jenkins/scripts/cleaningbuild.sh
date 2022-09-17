f6e2f795781080e22ef7efe57b1a6eb75e7b9281
Fajar-Islami <ahmadfajarislami@protonmail.com>
2022-09-17 20:46:38 +0700

    :hammer: update jenkinsfile

--- jenkins/scripts/cleaningbuild.sh
+++ jenkins/scripts/cleaningbuild.sh
@@ -8,24 +8,24 @@ set +x
 
 echo 'DONE........'
 
-echo 'Add to staging'
-set -x
-git config --global user.email "ahmadfajarislami@protonmail.com"
-git config --global user.name "Fajar-Islami"
-git add .
-set +x
+# echo 'Add to staging'
+# set -x
+# git config --global user.email "ahmadfajarislami@protonmail.com"
+# git config --global user.name "Fajar-Islami"
+# git add .
+# set +x
 
-echo 'Commit'
-set -x
-git commit -am 'push to heroku'
-set +x
+# echo 'Commit'
+# set -x
+# git commit -am 'push to heroku'
+# set +x
 
-echo 'Push to Heroku'
-set -x
-git remote -v
-git branch
-git push -u heroku HEAD:master
-set +x
+# echo 'Push to Heroku'
+# set -x
+# git remote -v
+# git branch
+# git push -u heroku HEAD:master
+# set +x
 
 
-echo 'DONE........'
+# echo 'DONE........'
